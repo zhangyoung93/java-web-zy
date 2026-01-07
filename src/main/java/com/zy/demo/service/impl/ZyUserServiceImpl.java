292ebc2f1530cfd9292d698f860a4bf864e8b9a2
zhangyoung93 <68599345+zhangyoung93@users.noreply.github.com>
2026-01-07 17:23:50 +0800

    fix(mybatis): batch insert add transaction manager

--- src/main/java/com/zy/demo/service/impl/ZyUserServiceImpl.java
+++ src/main/java/com/zy/demo/service/impl/ZyUserServiceImpl.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,13 +39,19 @@ public class ZyUserServiceImpl implements ZyUserService {
     @Override
     public Page<ZyUser> selectUserByPaging(int pageNum, int pageSize) {
         PageHelper.startPage(pageNum, pageSize);
-        List<ZyUser> zyUserList = this.zyUserMapper.selectByPaging(null,null);
+        List<ZyUser> zyUserList = this.zyUserMapper.selectByPaging(null, null);
         Page<ZyUser> page = (Page<ZyUser>) zyUserList;
         log.info("selectUserByPaging,page={}", page.toString());
         return page;
     }
 
+    /**
+     * Transactional注解将sql执行事务交给spring管理，无需手动提交、回滚、关闭资源
+     *
+     * @return int
+     */
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public int insertUser() {
         List<ZyUser> zyUserList = new ArrayList<>();
         ZyUser zyUser = new ZyUser();
@@ -59,28 +66,14 @@ public class ZyUserServiceImpl implements ZyUserService {
         zyUser.setLoginName("name2");
         zyUser.setLoginPwd("pwd2");
         zyUserList.add(zyUser);
-        //1、sqlSession批量执行
-        SqlSession sqlSession = null;
-        try {
-            //每1000条数据作为一个批次，分批次插入
-            List<List<ZyUser>> lists = Lists.partition(zyUserList, 1);
-            //创建数据库会话，执行模式为批量，关闭自动提交
-            sqlSession = this.sqlSessionFactory.openSession(ExecutorType.BATCH, false);
-            ZyUserMapper zyUserMapper = sqlSession.getMapper(ZyUserMapper.class);
-            for (List<ZyUser> list : lists) {
-                for (ZyUser user : list) {
-                    zyUserMapper.insertOne(user);
-                }
-            }
-            sqlSession.commit();
-        } catch (Exception e) {
-            if (sqlSession != null) {
-                sqlSession.rollback();
-            }
-            throw e;
-        } finally {
-            if (sqlSession != null) {
-                sqlSession.close();
+        //sqlSession批量插入。每1000条数据作为一个批次，分批次插入
+        List<List<ZyUser>> lists = Lists.partition(zyUserList, 1000);
+        //创建数据库会话，执行模式为批量，关闭自动提交
+        SqlSession sqlSession = this.sqlSessionFactory.openSession(ExecutorType.BATCH, false);
+        ZyUserMapper zyUserMapper = sqlSession.getMapper(ZyUserMapper.class);
+        for (List<ZyUser> list : lists) {
+            for (ZyUser user : list) {
+                zyUserMapper.insertOne(user);
             }
         }
         //2、<foreach标签>批量插入
