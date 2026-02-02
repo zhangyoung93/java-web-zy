707e391ace726f7bbf8159f330b22a1813d01320
zhangyoung93 <68599345+zhangyoung93@users.noreply.github.com>
2026-02-02 13:15:57 +0800

    feat(context): add ApplicationContext event listener

--- src/main/java/com/zy/demo/JavaWebDemoApplication.java
+++ src/main/java/com/zy/demo/JavaWebDemoApplication.java
@@ -1,14 +1,30 @@
 package com.zy.demo;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
+import com.zy.demo.bean.SpringBootBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * springboot启动类
- * MapperScan注解生成的mybatis代理对象底层依赖SqlSessionTemplate，线程安全
+ *
+ * <p>
+ * 注解@SpringBootApplication包含3个子注解：
+ * 1、@SpringBootConfiguration
+ * 声明启动类是SpringBoot配置类，可以注册Bean。
+ * 2、@EnableAutoConfiguration
+ * 开启Bean自动配置。加载自动配置的Bean。
+ * 从META-INF/spring.factories加载org.springframework.boot.autoconfigure.EnableAutoConfiguration
+ * 或者从META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports文件加载。
+ *
+ * 3、@ComponentScan
+ * 设置Bean扫描路径：扫描当前注解所在类的包及其所有子包
+ * <p>
+ *
+ * 注解@MapperScan生成的mybatis代理对象底层依赖SqlSessionTemplate，线程安全
  *
  * @author zy
  */
@@ -22,4 +38,8 @@ public class JavaWebDemoApplication {
         System.out.println("容器类型：" + context.getClass().getName());
     }
 
+    @Bean(initMethod = "init")
+    public SpringBootBean springBootBean() {
+        return new SpringBootBean();
+    }
 }
---
+++ src/main/java/com/zy/demo/bean/SpringBootBean.java
@@ -0,0 +1,16 @@
+package com.zy.demo.bean;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * SpringBootBean
+ *
+ * @author zy
+ */
+@Slf4j
+public class SpringBootBean {
+
+    private void init() {
+        log.info("SpringBootBean init");
+    }
+}
